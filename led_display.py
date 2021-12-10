89e295fe53fabf65eca766d759791ae67cb23f1e
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-10 21:11:08 +0000

    more

--- led_display.py
+++ led_display.py
@@ -6,16 +6,16 @@ class LEDdisplay():
   'Class for controlling a 7-segment LED display'
 
   numbers = [ 
-    0b10000001, # 0
-    0b01000001, # 1
-    0b00100001, # 2
-    0b00010001, # 3
-    0b00001001, # 4
-    0b00000101, # 5
-    0b00000011, # 6
-    0b00000001, # 7
-    0b00000001, # 8
-    0b00000001] # 9
+    0b10000000, # 0
+    0b11000000, # 1
+    0b10100000, # 2
+    0b10010000, # 3
+    0b10001000, # 4
+    0b10000100, # 5
+    0b10000010, # 6
+    0b10000001, # 7
+    0b10000000, # 8
+    0b00000000] # 9
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
