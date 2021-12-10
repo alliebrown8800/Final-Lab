d48c1ccd1981b25761264d40103bdece0ee8afaf
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-10 21:16:27 +0000

    numbers

--- led_display.py
+++ led_display.py
@@ -4,18 +4,19 @@ from shifter import Shifter    # extend by composition
 class LEDdisplay():
 
   'Class for controlling a 7-segment LED display'
-
+  # zero is ON, 1 is OFF
   numbers = [ 
-    0b10000000, # 0
-    0b11000000, # 1
-    0b10100000, # 2
-    0b10010000, # 3
-    0b10001000, # 4
-    0b10000100, # 5
+   #0b12345678 
+    0b11000000, # 0
+    0b11001111, # 1
+    0b10100100, # 2
+    0b10110000, # 3
+    0b10011001, # 4
+    0b10010010, # 5
     0b10000010, # 6
-    0b10000001, # 7
+    0b11111000, # 7
     0b10000000, # 8
-    0b00000000] # 9
+    0b10010000] # 9
 
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock)
