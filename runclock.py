5f4e65e4642bb516f3090ea36c34131c849d67c6
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-10 22:50:37 +0000

    wait

--- runclock.py
+++ runclock.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 from clock import Clock
+import time
 
 dataPin, latchPin, clockPin = 17, 27, 22
 digitPins = [6, 5, 13, 19]
 
 try: # exception handling
   ourClock = Clock(dataPin, latchPin, clockPin, digitPins)
+  time.sleep(120)
 
 # More exception handling:
 except KeyboardInterrupt: 
