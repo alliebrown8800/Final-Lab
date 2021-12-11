562ca527208de3a91bd2ff3e0ac01f71f1edd2ab
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-11 16:04:36 +0000

    a loop

--- testmotion.py
+++ testmotion.py
@@ -6,11 +6,12 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_MotionSensor, GPIO.IN)
 
 try: # exception handling
-  motion = GPIO.input(pin_MotionSensor) 
-  if motion == True:
-    print('Motion detected')
-  elif motion == False:
-    print('No motion detected')
+  while True:
+    motion = GPIO.input(pin_MotionSensor) 
+    if motion == True:
+      print('Motion detected')
+    elif motion == False:
+      print('No motion detected')
 
 # More exception handling:
 except KeyboardInterrupt: 
