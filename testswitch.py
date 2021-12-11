c97d4e7a192e1dc0892a7f08eebc379b5196929a
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-11 17:56:01 +0000

    test swt

--- testswitch.py
+++ testswitch.py
@@ -1,12 +1,24 @@
 import RPi.GPIO as GPIO
+import time
 
 switchPin = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switchPin, GPIO.IN)
 
-while True:
-  switch = GPIO.input(switchPin)
-  if switch == True:
-    print('switch is on')
-  elif switch == False:
-    print('switch is off')
+
+try: # exception handling
+  while True:
+    switch = GPIO.input(switchPin)
+    if switch == True:
+      print('switch is on')
+    elif switch == False:
+      print('switch is off')
+    time.sleep(1)
+
+# More exception handling:
+except KeyboardInterrupt: 
+  print('\nExiting')
+except Exception as e: # catch all other errors
+  print(e)
+
+GPIO.cleanup()
