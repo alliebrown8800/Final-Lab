9b2d0c43672732f7d13d34a02dae131cab2c857b
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-12-11 15:46:52 +0000

    test buzzer

--- main.py
+++ main.py
@@ -30,3 +30,13 @@ print(timeNow[0])
 for d in range(4):
   print(timeNow[d])
   time.sleep(0.001)
+
+minute = time.localtime().tm_min
+# Because the time comes up wrong:
+hour = time.localtime().tm_hour - 5
+if hour < 1: hour = hour + 24
+# Display non-military time:
+if hour > 12: hour = hour - 12
+# Making the time into a list of numbers:
+timeNow = str(hour) + str(minute)
+print(type(timeNow))
---
+++ testbuzzer.py
@@ -0,0 +1,23 @@
+import time
+from gpiozero import Buzzer
+
+buzzer = Buzzer(21)
+
+chosen_alarm = '1045'
+
+while True:
+  # Get the time:
+  minute = time.localtime().tm_min
+  # Because the time comes up wrong:
+  hour = time.localtime().tm_hour - 5
+  if hour < 1: hour = hour + 24
+  # Display non-military time:
+  if hour > 12: hour = hour - 12
+  # Making the time into a list of numbers:
+  timeNow = str(hour) + str(minute)
+
+  if chosen_alarm == timeNow:
+    buzzer.on()
+    time.sleep(5)
+    buzzer.off(5)
+    time.sleep(5)
